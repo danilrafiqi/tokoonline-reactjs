aed6ba0aef846e0f34bba0e0907ab6012cfc9de1
M Danil Rafiqi <danil.rafiqi@gmail.com>
2021-05-26 09:09:56 +0700

    token interceptor

--- src/commons/redux/middleware.js
+++ src/commons/redux/middleware.js
@@ -1,10 +1,20 @@
 import axios from "axios";
+import jwtDecode from "jwt-decode";
+import { authAction } from "./auth/slice";
 
 export const tokenInterceptor = (store) => (next) => (action) => {
   const state = store.getState();
   const { token } = state.auth;
 
-  if (token) axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+  if (token) {
+    const decodedToken = jwtDecode(token);
+
+    // clear token after the token expires
+    if (decodedToken.exp - new Date().getTime() / 1000 <= 0) {
+      next(authAction.updateToken(undefined));
+    }
+    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+  }
 
   return next(action);
 };
